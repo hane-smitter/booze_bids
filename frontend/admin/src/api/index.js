993d40139e425763d0cff4dd472341a40d5d4a43
hane-smitter <hanesmitter3@gmail.com>
2021-10-21 13:39:29 +0300

    merge

--- frontend/admin/src/api/index.js
+++ frontend/admin/src/api/index.js
@@ -15,9 +15,9 @@ export const updateProduct = (param, body) => elevateAxios.patch(`/products/admi
 });
 export const createProductBid = body => elevateAxios.post(`/products/admin/bid/create`, body);
 export const fetchProductCategories = () => axios.get(`/categories`);
-export const createProductCategory = body => axios.post(`/categories/admin`, body);//elevateAxios to be returned
-export const updateProductCategory = (param, body) => axios.patch(`/categories/admin/mod/update/${param}`, body);//elevateAxios to be returned
-export const deleteProductCategory = (body) => axios.delete(`/categories/admin/mod`, body);//elevateAxios to be returned
+export const createProductCategory = body => elevateAxios.post(`/categories/admin`, body);//elevateAxios to be returned
+export const updateProductCategory = (param, body) => elevateAxios.patch(`/categories/admin/mod/update/${param}`, body);//elevateAxios to be returned
+export const deleteProductCategory = (body) => elevateAxios.delete(`/categories/admin/mod`, body);//elevateAxios to be returned
 
 export const fetchBids = () => elevateAxios.get(`/bids/admin`);
 export const fetchExpiredBids = () => elevateAxios.get(`/bids/admin/expired`);
